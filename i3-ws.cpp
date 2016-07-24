3dd2a993d00159085ab93c2736f4f3396faf86c1
Michael Hoang <enzime@users.noreply.github.com>
2016-07-25 07:24:14 +1000

    Add support for absolute position

--- i3-ws.cpp
+++ i3-ws.cpp
@@ -1,29 +1,53 @@
 #include <i3ipc++/ipc.hpp>
+#include <algorithm>
 #include <cstring>
 #include <iostream>
-#include <algorithm>
+#include <sstream>
 
-typedef std::vector< std::shared_ptr<i3ipc::output_t> > i3_output_vector;
-typedef std::vector< std::shared_ptr<i3ipc::workspace_t> > i3_workspace_vector;
+using namespace std;
+using i3ipc::output_t;
+using i3ipc::workspace_t;
+using i3ipc::container_t;
+using i3ipc::rect_t;
+
+using con_ptr = shared_ptr<container_t>;
+using out_ptr = shared_ptr<output_t>;
+using ws_ptr = shared_ptr<workspace_t>;
+
+
+bool isPosInt(string input) {
+    string chars = "0123456789";
+    for (auto i : input) {
+        bool digit = false;
+        for (auto c : chars)
+            if (i == c)
+                digit = true;
+        if (!digit)
+            return false;
+    }
+
+    return true;
+}
 
 
 int main(int argc, char* argv[]) {
     // handle argv
-    std::vector<std::string> args(argv + 1, argv + argc);
+    vector<string> args(argv + 1, argv + argc);
 
-    bool prev = false;
     bool valid = true;
-    bool dirSet = false;
     bool loop = false;
-    std::string mode = "out";
+    int pos = 0; // -2 = next, -1 = prev, 0 = unset; 1-based index onwards
+    string mode = "out";
 
     for (auto it = args.begin(); it != args.end() && valid; ++it) {
-        if (*it == "prev") {
-            dirSet = true;
-            prev = true;
-        } else if (*it == "next")
-            dirSet = true;
-        else if (*it == "--ws")
+        if (*it == "prev" && !pos) {
+            pos = -1;
+        } else if (*it == "next" && !pos)
+            pos = -2;
+        else if (isPosInt(*it) && !pos) {
+            stringstream sstream(*it);
+            sstream >> pos;
+        } else if (*it == "--ws")
             mode = "ws";
         else if (*it == "--loop")
             loop = true;
@@ -32,8 +56,9 @@ int main(int argc, char* argv[]) {
     }
 
     // if no direction specified or invalid arguments
-    if (!valid || !dirSet) {
-        std::cout << "Usage: i3-ws [--ws] [--loop] (prev|next)" << std::endl;
+    if (!valid || !pos) {
+        cout << "Usage: i3-ws [--ws] [--loop] (prev|next|{NUMBER})";
+        cout << endl;
         return -1;
     }
 
@@ -42,32 +67,65 @@ int main(int argc, char* argv[]) {
 
     // get active outputs
     auto outputs = i3.get_outputs();
-    i3_output_vector active(outputs.size());
+    vector<out_ptr> active(outputs.size());
 
-    auto it = std::copy_if(outputs.begin(), outputs.end(), active.begin(), [](auto output){ return output->active; });
-    active.resize(std::distance(active.begin(), it));
+    auto it = copy_if(
+        outputs.begin(),
+        outputs.end(),
+        active.begin(),
+        [](auto output){ return output->active; }
+    );
+
+    active.resize(distance(active.begin(), it));
 
     // get focused workspace
     auto workspaces = i3.get_workspaces();
-    auto ws = std::find_if(workspaces.begin(), workspaces.end(), [](auto workspace){ return workspace->focused; });
+    auto ws = find_if(
+        workspaces.begin(),
+        workspaces.end(),
+        [](auto workspace){ return workspace->focused; }
+    );
 
     // get focused output
-    auto current_ouput = std::find_if(active.begin(), active.end(), [ws](auto output){ return output->name == (*ws)->output; });
+    auto current_ouput = find_if(
+        active.begin(),
+        active.end(),
+        [ws](auto output) {
+            return output->name == (*ws)->output; });
 
-    i3_workspace_vector current(workspaces.size());
+    vector<ws_ptr> current(workspaces.size());
 
-    auto end = std::copy_if(workspaces.begin(), workspaces.end(), current.begin(), [current_ouput](auto workspace){ return workspace->output == (*current_ouput)->name; });
-    current.resize(std::distance(current.begin(), end));
+    auto end = copy_if(
+        workspaces.begin(),
+        workspaces.end(),
+        current.begin(),
+        [current_ouput](auto workspace) {
+            return workspace->output == (*current_ouput)->name; });
+
+    current.resize(distance(current.begin(), end));
 
     // switch outputs
     if (active.size() > 1 && mode == "out") {
-        std::sort(active.begin(), active.end(), [](auto a, auto b) {
+        sort(active.begin(), active.end(), [](auto a, auto b) {
             return a->rect.x < b->rect.x;
         });
 
-        auto current_ouput = std::find_if(active.begin(), active.end(), [ws](auto output){ return output->name == (*ws)->output; });
+        auto current_ouput = find_if(
+            active.begin(),
+            active.end(),
+            [ws](auto output){ return output->name == (*ws)->output; }
+        );
 
-        auto active_index = std::distance(active.begin(), current_ouput) + (prev ? -1 : 1);
+        auto active_index = distance(active.begin(), current_ouput);
+
+        if (pos > 0 && pos - 1 != active_index)
+            active_index = pos - 1;
+        else if (pos == -1)
+            active_index = active_index - 1;
+        else if (pos == -2)
+            active_index = active_index + 1;
+        else
+            return 0;
 
         if (loop) {
             active_index = (active_index + active.size()) % active.size();
@@ -75,14 +133,25 @@ int main(int argc, char* argv[]) {
             return 0;
         }
 
-        auto n = active[active_index];
-        std::cout << n->name << std::endl;
-        i3.send_command("workspace " + n->current_workspace);
-    } else if (current.size() > 1 || mode == "ws") { // switch workspaces
-        auto cur = std::find(current.begin(), current.end(), *ws);
-        std::cout << (*cur)->name << std::endl;
+        cout << active_index << endl;
 
-        auto current_index = std::distance(current.begin(), cur) + (prev ? -1 : 1);
+        auto n = active[active_index];
+        cout << n->name << " " << n->current_workspace << endl;
+        i3.send_command("workspace " + n->current_workspace);
+    } else if (current.size() > 1 && mode == "ws") { // switch workspaces
+        auto cur = find(current.begin(), current.end(), *ws);
+        cout << (*cur)->name << endl;
+
+        auto current_index = distance(current.begin(), cur);
+
+        if (pos > 0 && pos - 1 != current_index)
+            current_index = pos - 1;
+        else if (pos == -1)
+            current_index = current_index - 1;
+        else if (pos == -2)
+            current_index = current_index + 1;
+        else
+            return 0;
 
         if (loop) {
             current_index = (current_index + current.size()) % current.size();
@@ -91,7 +160,7 @@ int main(int argc, char* argv[]) {
         }
 
         auto n = current[current_index];
-        std::cout << n->name << std::endl;
+        cout << n->name << endl;
         i3.send_command("workspace " + n->name);
     }
 
